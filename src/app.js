0314359aa982c887422122aa823594a70735ce15
Jerry299 <josephjeremiah564@yahoo.com>
2020-07-16 05:56:02 +0100

    created cards for meetings

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 let login = document.getElementById("log-In"); // get login
 let modal = document.getElementById("modal"); //get modal
 let closeBtn = document.getElementById("closebtn");
+let closeBtn2 = document.getElementById("closebtn2");
+const signUpModal = document.getElementById("signup-modal");
+const signUpBtn = document.getElementById("signUp");
 
 //define openModal
 const openModal = () => {
@@ -9,6 +12,15 @@ const openModal = () => {
 const closeModal = () => {
   modal.style.display = "none";
 };
+const openSignUpModal = () => {
+  signUpModal.style.display = "block";
+};
+const closeSignUpModal = () => {
+  signUpModal.style.display = "none";
+};
 
+signUpBtn.addEventListener("click", openSignUpModal);
+//signUpBtn.addEventListener("click", closeSignUpModal);
 login.addEventListener("click", openModal);
 closeBtn.addEventListener("click", closeModal);
+closeBtn2.addEventListener("click", closeSignUpModal);
