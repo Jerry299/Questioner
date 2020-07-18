c3d84c5f3f020ddc780ea2ccd3eaa0be0935da65
Jerry299 <josephjeremiah564@yahoo.com>
2020-07-18 06:43:56 +0100

    adding toggler

--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,4 @@
+//modal
 let login = document.getElementById("log-In"); // get login
 let modal = document.getElementById("modal"); //get modal
 let closeBtn = document.getElementById("closebtn");
@@ -24,3 +25,14 @@ signUpBtn.addEventListener("click", openSignUpModal);
 login.addEventListener("click", openModal);
 closeBtn.addEventListener("click", closeModal);
 closeBtn2.addEventListener("click", closeSignUpModal);
+//modal ends
+
+//toggler starts
+
+const navItems = document.querySelector(".nav-items");
+const toggler = document.querySelector(".toggler");
+// add a dark overlay to the page when toggler is clicked
+toggler.addEventListener("click", () => {
+  navItems.style.width = "200px";
+  console.log("AcTIVE");
+});
