c90687bfbbb516c0333e627fd70147f83a659bb8
Jerry299 <josephjeremiah564@yahoo.com>
2020-07-20 06:50:56 +0100

    added animation for responsive navbar

--- src/popup.js
+++ src/popup.js
@@ -1,38 +1,58 @@
 //modal
-let login = document.getElementById("log-In"); // get login
-let modal = document.getElementById("modal"); //get modal
-let closeBtn = document.getElementById("closebtn");
-let closeBtn2 = document.getElementById("closebtn2");
-const signUpModal = document.getElementById("signup-modal");
-const signUpBtn = document.getElementById("signUp");
+const modalFunction = () => {
+  let login = document.getElementById("log-In"); // get login
+  let modal = document.getElementById("modal"); //get modal
+  let closeBtn = document.getElementById("closebtn");
+  let closeBtn2 = document.getElementById("closebtn2");
+  const signUpModal = document.getElementById("signup-modal");
+  const signUpBtn = document.getElementById("signUp");
 
-//define openModal
-const openModal = () => {
-  modal.style.display = "block";
-};
-const closeModal = () => {
-  modal.style.display = "none";
-};
-const openSignUpModal = () => {
-  signUpModal.style.display = "block";
-};
-const closeSignUpModal = () => {
-  signUpModal.style.display = "none";
-};
+  //define openModal
+  const openModal = () => {
+    modal.style.display = "block";
+  };
+  const closeModal = () => {
+    modal.style.display = "none";
+  };
+  const openSignUpModal = () => {
+    signUpModal.style.display = "block";
+  };
+  const closeSignUpModal = () => {
+    signUpModal.style.display = "none";
+  };
 
-signUpBtn.addEventListener("click", openSignUpModal);
-//signUpBtn.addEventListener("click", closeSignUpModal);
-login.addEventListener("click", openModal);
-closeBtn.addEventListener("click", closeModal);
-closeBtn2.addEventListener("click", closeSignUpModal);
-//modal ends
+  signUpBtn.addEventListener("click", openSignUpModal);
+  //signUpBtn.addEventListener("click", closeSignUpModal);
+  login.addEventListener("click", openModal);
+  closeBtn.addEventListener("click", closeModal);
+  closeBtn2.addEventListener("click", closeSignUpModal);
+  //modal ends
+};
 
 //toggler starts
+const togglerFunction = () => {
+  const navItems = document.querySelector(".nav-items");
+  const toggler = document.querySelector(".toggler");
+  const navLinks = document.querySelectorAll(".nav-link p");
+  const navLinkMeetUp = document.querySelector(".nav-link #meetup a");
+  // add a dark overlay to the page when toggler is clicked
+  toggler.addEventListener("click", () => {
+    navItems.classList.toggle("nav-active");
+    // navLinkMeetUp.style.animation = `navMeetup 0.5s ease forwards`;
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navFade 0.5s ease forwards ${index / 1 + 2}`;
+      }
+    });
+    toggler.classList.toggle("burger-rotate");
+  });
+};
 
-const navItems = document.querySelector(".nav-items");
-const toggler = document.querySelector(".toggler");
-// add a dark overlay to the page when toggler is clicked
-toggler.addEventListener("click", () => {
-  navItems.style.width = "200px";
-  console.log("AcTIVE");
-});
+const app = () => {
+  modalFunction();
+  togglerFunction();
+};
+
+app();
